68375a47d7b0c82212ce7a94eb6b804475cc3801
tony trinh <tony.w.trinh3@gmail.com>
2020-07-04 21:33:14 -0700

    finished nav and header mobile

--- src/components/LandingPage/1Header.js
+++ src/components/LandingPage/1Header.js
@@ -5,13 +5,9 @@ import LandingForm from './1.1LandingForm';
 const Header=()=>{
     return (
         <header className="header">
-         
             <Navigation/>
             <LandingForm/>
-
             <div className="header__caption">Over 300 unique places <br/> to stay in San Francisco</div>
-
-
         </header>
     )
 };
