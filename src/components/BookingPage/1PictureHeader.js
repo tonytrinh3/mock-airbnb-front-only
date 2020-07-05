264fa5c0e01d7f4475bfe6f6a2e27cb15bd3bec8
tony trinh <tony.w.trinh3@gmail.com>
2020-07-04 23:18:04 -0700

    pic header patch

--- src/components/BookingPage/1PictureHeader.js
+++ src/components/BookingPage/1PictureHeader.js
@@ -8,14 +8,10 @@ class PictureHeader extends React.Component {
  
 
     updateImg(){
-    
         const {
             imgs,
             id
         } = this.props;
-
-        // const imgs=["img-1","img-1","img-1","img-1"];
-        // const id = 3;
 
         if(window.innerWidth <= 1100){ //1100px
            return (
@@ -31,13 +27,48 @@ class PictureHeader extends React.Component {
            );
         } else {
             return (
-                Object.values(imgs).map((img,i) =>{
-                    //console.log(img);
-                    return <img src={require(`../../img/booking-page/listing-${id}/${img}.jpg`)} alt= {`img_${i}`} className={`picture-header__img picture-header__img--${i}`} key ={`img_${i}`}/>
-                })
+                <React.Fragment className="picture-header__img-grid">
+                    {Object.values(imgs).map((img,i) =>{
+                        //console.log(img);
+                        return <img src={require(`../../img/booking-page/listing-${id}/${img}.jpg`)} alt= {`img_${i}`} className={`picture-header__img picture-header__img--${i}`} key ={`img_${i}`}/>
+                    })}
+                </React.Fragment>
             );
         }
     }
+
+    // renderImgGrid(){
+    //     const {
+    //         imgs,
+    //         id
+    //     } = this.props;
+    //     return(
+    //         <React.Fragment className="picture-header__img-grid">
+    //             {Object.values(imgs).map((img,i) =>{
+    //                 //console.log(img);
+    //                 return <img src={require(`../../img/booking-page/listing-${id}/${img}.jpg`)} alt= {`img_${i}`} className={`picture-header__img picture-header__img--${i}`} key ={`img_${i}`}/>
+    //             })}
+    //         </React.Fragment>
+    //     )
+    // };
+
+    // renderCarousel(){
+    //     const {
+    //         imgs,
+    //         id
+    //     } = this.props;
+    //     return(
+    //         <Carousel autoPlay className="picture-header__carousel">
+    //             {Object.values(imgs).map((img,i)=>{
+    //                 return (
+    //                     <div key = {i}>
+    //                         <img src={require(`../../img/booking-page/listing-${id}/${img}.jpg`)} alt= {`img_${i}`}  key ={`img_${i}`}/>
+    //                     </div>
+    //                 )
+    //             })}
+    //         </Carousel> 
+    //     )
+    // };
 
     componentDidMount(){
         this.updateImg(); 
@@ -51,6 +82,8 @@ class PictureHeader extends React.Component {
         return (
             <div className="picture-header margin-bottom-large">
                 {this.updateImg()}
+                {/* {this.renderImgGrid()}
+                {this.renderCarousel()} */}
             </div>
         )
 
