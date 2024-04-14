07ac1a1c52bd9dfc1a1e7fcd7afe6bb737ed89e3
Tony Trinh <tony@bokksu.com>
2024-04-14 14:26:44 -0400

    trips page

--- src/components/TripsPage/TripsPage.js
+++ src/components/TripsPage/TripsPage.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {fetchUserReservations} from '../../actions';
-import Navigation from '../Navigation';
-import Footer from '../Footer';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchUserReservations } from "../../actions";
+import Navigation from "../Navigation";
+import Footer from "../Footer";
 //import moment from 'moment';
 // import TripCard from './TripCard';
 
-class TripsPage extends React.Component{
-
-
-     //disabled this UNTIL express and mongodb to come into play
+class TripsPage extends React.Component {
+    //disabled this UNTIL express and mongodb to come into play
     // componentDidMount(){
     //     this.props.fetchUserReservations();
     //     // this.props.fetchBookings();
@@ -19,84 +17,79 @@ class TripsPage extends React.Component{
 
     // }
 
-    renderReservation(){
-
+    renderReservation() {
         const {
             userProfile,
-
-            tripsTEMP
-
+            tripsTEMP,
         } = this.props;
 
         console.log(tripsTEMP);
-        
+
         //disabled this UNTIL express and mongodb to come into play
-        return tripsTEMP.map((trip,i) =>{
-        // return trips.map((trip,i) =>{
+        return tripsTEMP?.map((trip, i) => {
+            // return trips.map((trip,i) =>{
 
-            const {
-                bookingId,
-                imgs,
-                endDate,
-                location,
-                startDate,
-                title
-            } = trip.reservation;
-
+            const { bookingId, imgs, endDate, location, startDate, title } = trip.reservation;
 
             //you do this to wait until userProfile is fetch and loading
-            if (!userProfile){
-                return null
+            if (!userProfile) {
+                return null;
             }
-            
-            if (trip.userId === userProfile.userId){
+
+            if (trip?.userId === userProfile?.userId) {
                 return (
-                    <div className="trip-card" key = {i}>
-                        <img className = "trip-card__img" src={require(`../../img/booking-page/listing-${bookingId}/${imgs.img_1}.jpg`)} alt= {`img_${i}`} key ={`img_${i}`}/>    
+                    <div className="trip-card" key={i}>
+                        <img
+                            className="trip-card__img"
+                            src={require(`../../img/booking-page/listing-${bookingId}/${imgs.img_1}.jpg`)}
+                            alt={`img_${i}`}
+                            key={`img_${i}`}
+                        />
                         <div className="trip-card__description ">
-                            <p className = "trip-card__description__date ">{`${startDate.split(" ")[0]} ${startDate.split(" ")[1]} - ${endDate.split(" ")[0]} ${endDate.split(" ")[1]}` }</p>                    
+                            <p className="trip-card__description__date ">{`${
+                                startDate.split(" ")[0]
+                            } ${startDate.split(" ")[1]} - ${endDate.split(" ")[0]} ${
+                                endDate.split(" ")[1]
+                            }`}</p>
                             {/* <h2 className="header-medium">{trip.reservation.description}</h2> */}
-                            <h3 className="trip-card__description__location ">{location.split(",")[0]}</h3>
+                            <h3 className="trip-card__description__location ">
+                                {location.split(",")[0]}
+                            </h3>
                             <p className="trip-card__description__title ">{title}</p>
-                            <p href="" className="trip-card__description__more-info">Show more trip plans</p>
+                            <p href="" className="trip-card__description__more-info">
+                                Show more trip plans
+                            </p>
                         </div>
-                      
+
                         {/* <p>{moment(trip.reservation.startDate).format("MMMM Do")} - {moment(trip.reservation.endDate).format("Do")} </p> */}
-                        
                     </div>
-                    
-                )
+                );
             }
-        })
+        });
     }
 
-
-    render(){
+    render() {
         //console.log(this.props.trips);
 
-        return(
-
+        return (
             <div className="trips-page">
-            <Navigation />
+                <Navigation />
                 <h1 className="trips-page__header header-big margin-bottom-large">Your Trips</h1>
-                <div className="trips-page__cards">
-                    {this.renderReservation()} 
-                </div>
-                <hr className="footer__hr hr"/>
-            <Footer/>
+                <div className="trips-page__cards">{this.renderReservation()}</div>
+                <hr className="footer__hr hr" />
+                <Footer />
             </div>
-
-        )
+        );
     }
-};
+}
 
-const mapStateToProps = (state) =>{
+const mapStateToProps = (state) => {
     console.log(state);
     return {
         trips: Object.values(state.userReservations),
         userProfile: state.auth.userProfile,
         tripsTEMP: Object.values(state.userResTEMP),
-    }
-}
+    };
+};
 
-export default connect(mapStateToProps,{fetchUserReservations})(TripsPage);
+export default connect(mapStateToProps, { fetchUserReservations })(TripsPage);
