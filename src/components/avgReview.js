7be430729e3f79a93b0a78f4a277b2f990edf586
Tony Trinh <tony@bokksu.com>
2024-04-14 22:21:01 -0400

    fix eval

--- src/components/avgReview.js
+++ src/components/avgReview.js
@@ -1,11 +1,10 @@
-export const avgReview = (reviews,type) =>{
+export const avgReview = (reviews, type) => {
     //this could be in a separate function file
-        //console.log(reviews);
-        let sum = 0;
-        reviews.map((review)=>{
-            // return sum += eval("review." + type)
-            return review;
-        });
-      return Math.round((sum/reviews.length)*10)/10;  
-};
+    // console.log(reviews);
+    let sum = 0;
+    reviews.forEach((review) => {
+        sum += review[type];
+    });
 
+    return Math.round((sum / reviews.length) * 10) / 10;
+};
